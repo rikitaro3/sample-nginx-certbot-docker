12440a7ce917577bb34af813c65aa08a634fa97a
rikitaro <ohhata.rikiya@gmail.com>
2024-02-07 04:57:28 +0000

    fix: cert export dir

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# nginxの起動
+sudo docker-compose up -d --build
+
+# 証明書の発行
 sudo docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d example.com --register-unsafely-without-email
+
+# app.confのSSL通信設定コメントアウト除外
+
+# nginxリロード
+sudo docker exec nginx_sample nginx -s reload
